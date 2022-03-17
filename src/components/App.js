7ce334d735095976d0642f0f7d722db9dcf2bb24
John Miller <johnlukemiller@gmail.com>
2022-03-16 20:05:36 -0700

    Updates overall layout to resize better on various screens, adds placeholder for future menu dropdown

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,11 @@ class App extends Component {
           <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
         </Helmet>
         <header className="App-header">
-          Shramble
+          {/* Put Menu icon here */}
+          <div>-</div>
+          <div className="title">
+            Shramble
+          </div>
         </header>
         <div className='content'>
           <Outlet />
